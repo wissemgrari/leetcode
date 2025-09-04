4f7ab97f5fa9f03a1cf5b56e6fda954829fce481
wissemgrari <wissemgrari@outlook.com>
2025-09-04 18:39:00 +0100

    palindrome-number

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,19 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
 
-  public int[] twoSum(int[] nums, int target) {
-    Map<Integer, Integer> map = new HashMap<>();
-    for (int i = 0; i < nums.length; i++) {
-      int diff = target - nums[i];
-      if (map.containsKey(diff)) {
-        return new int[]{i, map.get(diff)};
-      }
-      map.put(nums[i], i);
-
+  public boolean isPalindrome(int x) {
+    if (x < 0) {
+      return false;
     }
-    return nums;
+    if (x >= 0 && x < 10) {
+      return true;
+    }
+
+    int rev = 0;
+    int num = x;
+    while (num != 0) {
+      rev = rev * 10 + num % 10;
+      num = num / 10;
+    }
+    return rev == x;
   }
 }
