4b897d124b475801eaee3eba535784c5c3cb1e19
wissemgrari <wissemgrari@outlook.com>
2025-09-04 18:34:42 +0100

    roman-to-integer

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,30 @@
+import java.util.Map;
+
 public class Solution {
-  // solution goes here.
+
+  public int romanToInt(String s) {
+    Map<Character, Integer> romans = Map.of(
+        'I', 1,
+        'V', 5,
+        'X', 10,
+        'L', 50,
+        'C', 100,
+        'D', 500,
+        'M', 1000
+    );
+
+    int result = 0;
+    int prev = 0;
+
+    for (int i = s.length() - 1; i >= 0; i--) {
+      int current = romans.get(s.charAt(i));
+      if (current < prev) {
+        result -= current;
+      } else {
+        result += current;
+      }
+      prev = current;
+    }
+    return result;
+  }
 }
