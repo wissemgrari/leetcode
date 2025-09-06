f6e0c09edff17ae78668408cfa52afa38da60359
wissemgrari <wissemgrari@outlook.com>
2025-09-06 09:29:14 +0100

    longest-common-prefix

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,17 @@
 public class Solution {
-  // solution goes here.
+
+  public String longestCommonPrefix(String[] strs) {
+    String first = strs[0];
+    int end = first.length();
+    for (int i = 1; i < strs.length && end > 0; i++) {
+      String s = strs[i];
+      int j = 0;
+      int limit = Math.min(end, s.length());
+      while (j < limit && first.charAt(j) == s.charAt(j)) {
+        j++;
+      }
+      end = j;
+    }
+    return first.substring(0, end);
+  }
 }
